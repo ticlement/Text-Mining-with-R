796150ec981ad78aa391c1e7e95b9cdc23533b17
bryelandt <brieuc.ryelandt@gmail.com>
2018-11-16 21:11:16 +0100

    code cleaning

--- LSA/BrouillonPUBMED.R
+++ LSA/BrouillonPUBMED.R
@@ -65,7 +65,6 @@ df <- df[nchar(as.character(df[ , 2]))<3000 & nchar(as.character(df[ , 2]))>100,
 # plot(a)
 # rm(a)
 
-
 ############### PART 2: text mining  ###############
 
 ## Bag of words approach:
@@ -100,64 +99,7 @@ tokens <- tokens_select(tokens,min_nchar = 3, selection ="keep")
 # print(tokens)
 
 # Create our first bag-of-words model dataframe.
-# tokens <- tokens[1:(length(tokens)/2)]
-tokens.dfm <- dfm(tokens)
-tokens.matrix <- tokens.dfm
-# Transform to a matrix and inspect.
-# tokens.matrix <- big.matrix(tokens.dfm) --> 14.5 Go (no stem)
-# library(Matrix)
-# isValue <- tokens.dfm != 0
-# iValue <- rowSums(isValue)
-# isparse <- vector(length = sum(isValue))
-# k <- 1
-# for (i in (1:dim(tokens.dfm)[1])){
-#   l <- k + iValue[i] -1
-#   isparse[k:l] <- i
-#   k <- k + iValue[i]
-# }
-# 
-# jValue <- colSums(isValue)
-# jsparse <- vector(length = sum(isValue))
-# k <- 1
-# for (i in (1:dim(tokens.dfm)[2])){
-#   l <- k + jValue[i] -1
-#   jsparse[k:l] <- i
-#   k <- k + jValue[i]
-# }
-# 
-# indcs = which(isValue == TRUE)
-# Data <- tokens.dfm[indcs, , drop=F]
-# Data <- as.big.matrix(tokens.dfm[isValue])
-# 
-# sparseTokens <- sparseMatrix(isparse,jsparse,x=Data)
-# 
-# tokens.matrix <- sparseTokens
-# Tokenfrequence visualizations
-# In corpus
-# freq <- sort(colSums(tokens.matrix), decreasing=TRUE)
-# wf <- data.frame(word=names(freq), freq=freq)
-# 
-# # In specific document
-# Doc<-5
-# freqInDoc <- sort(tokens.matrix[Doc,], decreasing=TRUE)
-# wfindoc <- data.frame(word=names(freqInDoc), freq=freqInDoc)
-
-# plot word frequence
-# in corpus:
-# pl <- ggplot(subset(wf, freq > 1) ,aes(word, freq))
-
-# in specific doc:
-# # pl <- ggplot(subset(wfindoc, freq > 1) ,aes(word, freq))
-
-# make plot:
-# pl <- pl + geom_bar(stat="identity", fill="darkred", colour="white")
-# pl + theme(axis.text.x=element_text(angle=90, hjust=1)) + ggtitle("Uni-Gram Frequency")
-
-# Word Cloud visualization
-# library(wordcloud)
-# set.seed(100)
-# wordcloud(names(freq), freq, min.freq=2, colors=brewer.pal(6, "Dark2"))
-
+tokens.matrix <- dfm(tokens)
 
 ## analizing Tokens:
 #-------------------
@@ -179,68 +121,28 @@ tf.idf <- function(tf, idf) {
   tf * idf
 }
 
-
 # First step, normalize all documents via TF.
-# tokens.df <- apply(tokens.matrix, 1, term.frequency) # -> breaks sparse matrix
 tokens.tf <- term.frequency(tokens.matrix)
 
 # Second step, calculate the IDF vector that we will use - both
-# tokens.idf <- apply(tokens.matrix, 2, inverse.doc.freq)
 tokens.idf <- inverse.doc.freq(tokens.matrix)
 
 # Lastly, calculate TF-IDF for our training corpus.
-# tokens.tfidf <-  apply(tokens.df, 2, tf.idf, idf = tokens.idf)
 tokens.tfidf <- tf.idf(tokens.tf,tokens.idf)
-
-# Transpose the matrix
-# tokens.tfidf <- t(tokens.tfidf)
-
-# Check for incopmlete cases.
-# incomplete.cases <- which(!complete.cases(tokens.tfidf)) -> doesn't work on sparse matrix
-# Abstract[incomplete.cases]
-
-# Fix incomplete cases
-# tokens.tfidf[incomplete.cases,] <- rep(0.0, ncol(tokens.tfidf))
-# dim(tokens.tfidf)
-# sum(which(!complete.cases(tokens.tfidf)))
-
-# Make a clean data frame.
-# tokens.tfidf.df <- as.data.frame(as.matrix(tokens.tfidf))
-# names(tokens.tfidf.df) <- make.names(names(tokens.tfidf.df))
-
-
 
 ## Perform SVD. Specifically, reduce dimensionality down to 'nv' columns
 #-----------------------------------------------------------------------
-ptm <- proc.time()
+
 library(irlba)
 
-# for our latent semantic analysis (LSA).
 irlba <- irlba(tokens.tfidf, nv = 100, maxit = 1000)
-proc.time() - ptm
-## Make plots:
 
 # line names
 rownames(irlba$v) <- colnames(tokens.dfm)
 rownames(irlba$u) <- row.names(tokens.dfm)
 
-# 2D Plot:
-# plot(eig1,eig2,col="blue")
-# text(eig1,eig2,row.names(irlba$v), cex=0.6, pos=4, col="red")
-
-# 3D Plot:
-# library("scatterplot3d")
-
-# s3d<-scatterplot3d(eig1,eig2,eig3, pch = 16, color="steelblue")
-# text(s3d$xyz.convert(eig1,eig2,eig3), labels = rownames(irlba$v),
-#      cex= 0.7, col = "red")
-
-# library(car) # faut aussi installer lib("rgl")
-# # 3D plot with the regression plane
-# scatter3d(x = eig1, y = eig2, z = eig3)
-
-
 # topics Visualization
+
 Topics <- vector(length = dim(irlba$v)[1])
 TopicsWords <- matrix("txt",nrow = 50, ncol = dim(irlba$v)[2])
 ColTag <- vector(length = dim(irlba$v)[2])
@@ -257,20 +159,21 @@ colnames(TopicsWords) <- ColTag
 View(TopicsWords)
 
 
-############### Querries (based on SVD) ###############
+############### Queries (based on SVD) ###############
 #------------------------------------------------------
 
-# give a positive querry: as a vector of strings ('querry','querry',...)
-posQuerry_String <- c('cancer')
-posQuerry_String <- stemDocument(posQuerry_String)
+# give a positive query: as a vector of strings ('querry','querry',...)
+posQuerry_String <- c('cancer','pancreatic')
+# posQuerry_String <- stemDocument(posQuerry_String) # IF STEMMING
 flag <- match(posQuerry_String, rownames(irlba$v))
-try(if(is.na(flag) == TRUE) stop("Query not found"))
-# give a negative querry:
-negQuerry_String <- c('pancreatic')
-negQuerry_String <- stemDocument(negQuerry_String)
+try(if(sum(is.na(flag)) > 0) stop("Query not found"))
+# give a negative query:
+negQuerry_String <- c('')
+# negQuerry_String <- stemDocument(negQuerry_String) # IF STEMMING
 flag <- match(negQuerry_String, rownames(irlba$v))
-try(if(is.na(flag) == TRUE) stop("Query not found"))
+try(if(negQuerry_String != '' & sum(is.na(flag)) > 0) stop("Query not found"))
 
+# Compute the queries' coordinates in SVD matrix
 posIndex <- vector(length = length(posQuerry_String))
 for (i in (1:length(posQuerry_String))) {
   posIndex[i] <- match(posQuerry_String[i], rownames(irlba$v))
@@ -285,13 +188,7 @@ if (length(negQuerry_String)>1 | negQuerry_String != ''){
   eig_negQuerry <- irlba$v[negIndex,]
 }
 
-# 
-# s3d<-scatterplot3d(eig1,eig2,eig3, pch = 16, color="steelblue")
-# text(s3d$xyz.convert(eig1,eig2,eig3), labels = rownames(irlba$v),
-#      cex= 0.7, col = "green")
-# s3d$points3d(eig_posQuerry[1],eig_posQuerry[2],eig_posQuerry[3],pch=16,color="red")
-
-# This function computes the euclidean distance between the querry and each document
+# This function computes the euclidean distance between the queries and each document
 euc.dist <- function(docs,querry){ 
   dimDocs <- dim(docs)
   squareSum <- 0
@@ -305,6 +202,7 @@ euc.dist <- function(docs,querry){
   }
   return(euc.dist)
 }
+
 # Calculate distance, order and name the rows
 posdistMatrix <- matrix(nrow = length(posQuerry_String),ncol=dim(irlba$u)[1])
 posdist <- rep(1,length = dim(irlba$u)[1])
@@ -331,28 +229,3 @@ names(distMatrix) <- rownames(irlba$u)
 distMatrix <- distMatrix[order(distMatrix),drop=FALSE]
 
 head(distMatrix)
-
-# Compute 10 most relevant (tf-idf) words in each documents
-# bestWords <- function(tokens.tfidf,docId,irlba){
-#   docTfidf <- tokens.tfidf[docId,]
-#   names(docTfidf) <- rownames(irlba$v)
-#   # docTfidf <- docTfidf[is.na(docTfidf) == FALSE]
-#   docTfidf <- docTfidf[order(-docTfidf),drop=FALSE]
-#   return(names(docTfidf[1:10]))
-# }
-# 
-# # matrix with 10 most relevant keywords of the 10 nearests documents 
-# names <- names(distMatrix)
-# Result <- vector(nrow = 10,ncol = 10)
-# colnames(Result) <- names[1:10]
-# for (i in (1:10)) {
-#   index <- match(names[i],rownames(irlba$v))
-#   Result[i,] <- bestWords(tokens.tfidf,index,irlba)
-# }
-# View(Result)
-
-# Clustering ?
-#--------------
-# res <- kmeans(irlba$v,centers = 2)
-# plot(irlba$v,col = res$cluster , pch = res$cluster)
-# points(res$centers, col = 1:5, pch = 8)
